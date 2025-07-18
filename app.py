d6a391bf98d1ffe1a3153b840619df830ffc9643
MeijerW <110025532+MeijerW@users.noreply.github.com>
2025-07-18 15:32:56 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Gene Explorer", layout="wide")
+
+#load data
+#make heatmap
