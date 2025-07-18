bd056391c9d75e098127587337c649227b1decb3
MeijerW <110025532+MeijerW@users.noreply.github.com>
2025-07-18 15:35:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Gene Explorer", layout="wide")
 
 #load data
+df = pd.read_csv("https://raw.githubusercontent.com/MeijerW/ProteomeUI/edit/main/Datafiles/my_expression_data.csv")
+
 #make heatmap
